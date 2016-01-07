ff06e9556c5250529cb5f3eab7a7a4c106386f01
root <day7pettersens@gmail.com>
2016-01-08 00:14:01 +0700

    add assets dir and engine

---
+++ lib/bucnewswidget/engine.rb
@@ -0,0 +1,9 @@
+module Bucnewswidget 
+  class Engine < ::Rails::Engine
+    NAME = 'bucnewswidget'
+    include NA::EngineConfig
+    config.to_prepare do
+      IMPORT_EXPORT_EXTENSIONS << 'bucnewswidget'
+    end
+  end
+end
